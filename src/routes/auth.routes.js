a0af5d6291870643b5c6f6f1104592062a570c13
Snigdha-abes <snigdhasharma0905@gmail.com>
2025-10-06 21:40:33 +0530

    Backend completed

--- src/routes/auth.routes.js
+++
@@ -1,6 +0,0 @@
-const express=require("express")
-const router=express.Router()
-const authControllers=require("../controllers/auth.controller")
-router.post('/register',authControllers.registerUser)
-router.post('/login',authControllers.loginUser)
-module.exports=router
